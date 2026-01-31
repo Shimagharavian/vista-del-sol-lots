d26befa361e6c461e51a8cfd1bfaf7c4569b8b96
Shimagharavian <sh.gharavian@gmail.com>
2026-01-30 21:25:36 -0800

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "40px" }}>
+      <h1>Vista del Sol</h1>
+      <p>Interactive lot sales website coming soon.</p>
+    </main>
+  );
+}
